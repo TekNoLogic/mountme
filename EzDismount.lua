b0a924bcf0902e5e5a6b07da7e12128fc2fa12c6
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-05-29 06:55:01 +0000

    MountMe - Ripped out a bunch of crap, including the mountup logics.  USE A NORMAL MACRO!  MountMe is going to only handle item swapping and perhaps selective de-shifting.  2.1.0's autodismount is everything MM's dismounter had wished to be.
    git-svn-id: http://tekkub-wow.googlecode.com/svn/trunk/MountMe@412 86fe6d9a-1522-0410-a387-bf9db416f0a0

--- EzDismount.lua
+++ EzDismount.lua
@@ -8,25 +8,10 @@ local L = {
 	Dismount = true,
 	["Options for Dismounting."] = true,
 
-	dazed = true,
-	["Clear when Dazed"] = true,
-	["Removes Aspect of the Cheetah/Pack when Dazed."] = true,
-
-	taxi = true,
-	["Dismount at flightmaster"] = true,
-	["Dismounts when the flight map is opened."] = true,
-
 	deshift = true,
 	["Deshift on error"] = true,
 	["Clears shapeshifts on \"Can't do that when shapeshifted\" errors."] = true,
 
-	dismount = true,
-	["Dismount on error"] = true,
-	["Dismounts on \"Can't do that when mounted\" errors."] = true,
-
-	stand = true,
-	["Stand on error"] = true,
-	["Stand on \"You must be standing to do that\" errors."] = true,
 }
 for i,v in pairs(L) do if v == true then L[i] = i end end -- Too lazy to copy/paste right now
 
@@ -36,17 +21,6 @@ for i,v in pairs(L) do if v == true then L[i] = i end end -- Too lazy to copy/pa
 ------------------------------
 
 local _, myclass = UnitClass("player")
-local standerrors = {
-	[ERR_CANTATTACK_NOTSTANDING] = true,
-	[ERR_LOOT_NOTSTANDING] = true,
-	[ERR_TAXINOTSTANDING] = true,
-	[SPELL_FAILED_NOT_STANDING] = true,
-}
-local mounterrors = {
-	[ERR_ATTACK_MOUNTED] = true,
-	[SPELL_FAILED_NOT_MOUNTED] = true,
-	[PLAYER_LOGOUT_FAILED_ERROR] = true,
-}
 local shifterrors = {
 	[ERR_CANT_INTERACT_SHAPESHIFTED] = true,
 	[ERR_MOUNT_SHAPESHIFTED] = true,
@@ -67,9 +41,6 @@ MountMeEzDismount = MountMe:NewModule("EZDismount", "AceEvent-2.0", "AceDebug-2.
 MountMe:RegisterDefaults("EZDismount", "profile", {
 	taxi = true,
 	deshift = true,
-	dismount = true,
-	stand = true,
-	clearondaze = true,
 })
 MountMeEzDismount.db = MountMe:AcquireDBNamespace("EZDismount")
 MountMeEzDismount.consoleCmd = L["dismount"]
@@ -78,13 +49,6 @@ MountMeEzDismount.consoleOptions = {
 	name = L["Dismount"],
 	desc = L["Options for Dismounting."],
 	args = {
-		[L["taxi"]] = {
-			type = "toggle",
-			name = L["Dismount at flightmaster"],
-			desc = L["Dismounts when the flight map is opened."],
-			get = function() return MountMeEzDismount.db.profile.taxi end,
-			set = function(v) MountMeEzDismount.db.profile.taxi = v end,
-		},
 		[L["deshift"]] = {
 			type = "toggle",
 			name = L["Deshift on error"],
@@ -92,20 +56,6 @@ MountMeEzDismount.consoleOptions = {
 			get = function() return MountMeEzDismount.db.profile.deshift end,
 			set = function(v) MountMeEzDismount.db.profile.deshift = v end,
 			hidden = myclass ~= "DRUID" and myclass ~= "SHAMAN",
-		},
-		[L["dismount"]] = {
-			type = "toggle",
-			name = L["Dismount on error"],
-			desc = L["Dismounts on \"Can't do that when mounted\" errors."],
-			get = function() return MountMeEzDismount.db.profile.dismount end,
-			set = function(v) MountMeEzDismount.db.profile.dismount = v end,
-		},
-		[L["stand"]] = {
-			type = "toggle",
-			name = L["Stand on error"],
-			desc = L["Stand on \"You must be standing to do that\" errors."],
-			get = function() return MountMeEzDismount.db.profile.stand end,
-			set = function(v) MountMeEzDismount.db.profile.stand = v end,
 		},
 	}
 }
@@ -116,7 +66,7 @@ MountMeEzDismount.consoleOptions = {
 ---------------------------
 
 function MountMeEzDismount:OnEnable()
-	self:RegisterEvent("TAXIMAP_OPENED")
+--~ 	self:RegisterEvent("TAXIMAP_OPENED")
 	self:RegisterEvent("UI_ERROR_MESSAGE")
 end
 
@@ -127,15 +77,13 @@ end
 
 function MountMeEzDismount:UI_ERROR_MESSAGE(msg)
 	if UnitOnTaxi("player") then return
-	elseif mounterrors[msg] and self.db.profile.dismount then return Dismount()
-	elseif shifterrors[msg] and self.db.profile.deshift then return self:TriggerEvent("MountMe_Deshift")
-	elseif standerrors[msg] and self.db.profile.stand then return SitOrStand() end
+	elseif shifterrors[msg] and self.db.profile.deshift then return self:TriggerEvent("MountMe_Deshift") end
 end
 
 
-function MountMeEzDismount:TAXIMAP_OPENED()
-	if self.db.profile.taxi then Dismount() end
-end
+--~ function MountMeEzDismount:TAXIMAP_OPENED()
+--~ 	if self.db.profile.taxi then Dismount() end
+--~ end
 
 
 
--- MountMe.lua
+++ MountMe.lua
@@ -1,98 +1,39 @@
 ﻿
-BINDING_HEADER_MOUNTME = "MountMe"
-
-
 ----------------------------
 --      Localization      --
 ----------------------------
 
-local L = {
-	now = "now",
-	["Mount Me Now!"] = "Mount Me Now!",
-	["Mounts with the best available mount or travel form"] = "Mounts with the best available mount or travel form",
-}
+local L = {}
 
 
 ------------------------------
 --      Are you local?      --
 ------------------------------
 
--- AceLibrary("SpecialEvents-Equipped-2.0") -- For InBed
-local pt = AceLibrary("PeriodicTable-2.0")
-local selearn = AceLibrary("SpecialEvents-LearnSpell-2.0")
-local semove = AceLibrary("SpecialEvents-Movement-2.0")
-local seaura = AceLibrary("SpecialEvents-Aura-2.0")
-local semount = AceLibrary("SpecialEvents-Mount-2.0")
 local BS = AceLibrary("Babble-Spell-2.2")
-
-local frame
 local _, myclass = UnitClass("player")
-local spellmounts = {[BS["Summon Felsteed"]] = 60, [BS["Summon Warhorse"]] = 60, [BS["Summon Dreadsteed"]] = 100, [BS["Summon Charger"]] = 100}
-
-
-local function SetManyAttributes(self, ...)
-	for i=1,select("#", ...),2 do
-		local att,val = select(i, ...)
-		if not att then return end
-		self:SetAttribute(att,val)
-	end
-end
+local mounted, flying
 
 
 -------------------------------------
 --      Namespace Declaration      --
 -------------------------------------
 
---~ MountMe = AceLibrary("AceAddon-2.0"):new("AceEvent-2.0", "AceConsole-2.0", "AceDB-2.0")
---~ MountMe:RegisterDB("MountMeDB", "MountMeDBPerChar")
---~ MountMe.cmdtable = {type = "group", handler = MountMe, args = {}}
---~ MountMe:RegisterChatCommand({"/mme", "/mountme"}, MountMe.cmdtable)
-
-MountMe = DongleStub("Dongle-1.0"):New("MountMe")
-
-
----------------------------
---      Ace Methods      --
----------------------------
-
-function MountMe:Initialize()
-	frame = CreateFrame("Button", "MountMeFrame", UIParent, "SecureActionButtonTemplate")
-
-	frame.SetManyAttributes = SetManyAttributes
-	frame:EnableMouse(true)
-	frame:SetScript("PreClick", MountMe.PreClick)
-	frame:Hide()
-
---~ 	frame:SetMovable(true)
---~ 	frame:SetPoint("CENTER", UIParent, "CENTER",-150,-150)
---~ 	frame:SetWidth(50)
---~ 	frame:SetHeight(50)
---~ 	frame:SetBackdrop({
---~ 		bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background", tile = true, tileSize = 16,
---~ 		edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border", edgeSize = 16,
---~ 		insets = {left = 4, right = 4, top = 4, bottom = 4},
---~ 	})
-
-
---~ 	for name,module in self:IterateModules() do
---~ 		if module.consoleOptions then
---~ 			self.cmdtable.args[module.consoleCmd] = module.consoleOptions
---~ 		end
---~ 	end
-end
-
-
-function MountMe:Enable()
-	self:RegisterEvent("ZONE_CHANGED_NEW_AREA", SetMapToCurrentZone)
-	self:RegisterEvent("MountMe_Deshift")
-end
+local mm = DongleStub("Dongle-1.0"):New("MountMe")
+MountMe = mm
 
 
 ------------------------------
 --      Event Handlers      --
 ------------------------------
 
-function MountMe:MountMe_Deshift()
+function mm:Enable()
+	self:RegisterEvent("ZONE_CHANGED_NEW_AREA", SetMapToCurrentZone)
+	self:RegisterEvent("MountMe_Deshift")
+end
+
+
+function mm:MountMe_Deshift()
 	if myclass == "DRUID" then
 		for i=1,GetNumShapeshiftForms() do
 			local _, _, active = GetShapeshiftFormInfo(i)
@@ -111,73 +52,24 @@ function MountMe:MountMe_Deshift()
 end
 
 
-------------------------------
---      Mount handling      --
-------------------------------
+-------------------------------
+--      Mount Detection      --
+-------------------------------
 
-function MountMe:GetRandomMount()
-	if UnitLevel("player") < 40 then return end
+local f = CreateFrame("Frame")
+f.name = "MountMeMountEvent"
+f:SetScript("OnUpdate", function()
+	local m, f = IsMounted(), IsFlying()
+	if m == mounted and f == flying then return end
 
-	local norms, epics = {}, {}
-	local found = {[60] = norms, [100] = epics}
+	if m and not mounted then mm:TriggerMessage("MountMe_Mounted")
+	elseif not m and mounted then mm:TriggerMessage("MountMe_Dismounted") end
 
-	if GetZoneText() == "Ahn'Qiraj" then
-		for bag,slot,val in pt:BagIter("Mounts - AQ") do
-			local name = GetItemInfo(GetContainerItemLink(bag, slot))
-			self:Debug(1, "Found AQ mount", name)
-			table.insert(found[val], name)
-		end
-	end
+	if f and not flying then mm:TriggerMessage("MountMe_Flying")
+	elseif not f and flying then mm:TriggerMessage("MountMe_Landed") end
 
-	if #epics == 0 then
-		for bag,slot,val in pt:BagIter("Mounts") do
-			local name = GetItemInfo(GetContainerItemLink(bag, slot))
-			self:Debug(1, "Found mount", name)
-			table.insert(found[val], name)
-		end
+	flying = f
+	mounted = m
+end)
 
-		for name,speed in pairs(spellmounts) do
-			if selearn:SpellKnown(name) then
-				self:Debug(1, "Found spell", name)
-				table.insert(found[speed], name)
-			end
-		end
-	end
-
-	self:Debug(1, "Epics found", #epics, "Norms found", #norms)
-	if #epics > 0 then return epics[math.random(#epics)] end
-	if #norms > 0 then return norms[math.random(#norms)] end
-end
-
-
-function MountMe.PreClick(button, down)
-	local lvl = UnitLevel("player")
-	if InCombatLockdown() then return end
-
-	if IsSwimming() then
-		if myclass == "DRUID" and UnitLevel("player") >= 16 then CastShapeshiftForm(2)
-		elseif GetItemInfo(GetInventoryItemLink("player", 16)) == "Hydrocane" then frame:SetAttribute("type1", ATTRIBUTE_NOOP)
-		else frame:SetManyAttributes("type1", "macro", "macrotext", "/equip Hydrocane") end
-	elseif IsMounted() then
-		Dismount()
-		frame:SetAttribute("type1", ATTRIBUTE_NOOP)
-	elseif MountMe:MountMe_Deshift() then frame:SetAttribute("type1", ATTRIBUTE_NOOP)
-	elseif myclass == "SHAMAN" and lvl < 40 and lvl >= 20 then frame:SetManyAttributes("type1", "spell", "spell", BS["Ghost Wolf"])
-	elseif semove:PlayerMoving() then
-		if myclass == "DRUID" and UnitLevel("player") >= 30 then CastShapeshiftForm(4)
-		elseif myclass == "HUNTER" and UnitLevel("player") then
-			if not GetPlayerBuffName("player", BS["Aspect of the Cheetah"]) and selearn:SpellKnown(BS["Aspect of the Cheetah"]) then
-				frame:SetManyAttributes("type1", "spell", "spell", BS["Aspect of the Cheetah"])
-			elseif selearn:SpellKnown(BS["Aspect of the Pack"]) then frame:SetManyAttributes("type1", "spell", "spell", BS["Aspect of the Pack"])
-			else frame:SetAttribute("type1", ATTRIBUTE_NOOP) end
-		else frame:SetAttribute("type1", ATTRIBUTE_NOOP) end
-	else
-		-- Use our mount item
-		local rand = MountMe:GetRandomMount()
-		if not rand then return end
-		local isspell = spellmounts[rand]
-		frame:SetAttribute("type1", isspell and "spell" or "item")
-		frame:SetAttribute(isspell and "spell" or "item", rand)
-	end
-end
 
